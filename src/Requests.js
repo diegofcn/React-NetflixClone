344b8ed32012ecf4c252a9feed61b38313cb7cb1
diegofcn <94481027+diegofcn@users.noreply.github.com>
2022-05-20 11:29:12 +0200

    first upload

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '30fcee0df6b2f68bc94d7a1fcd04c6de'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
